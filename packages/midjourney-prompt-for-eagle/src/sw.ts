7d2d6c41d7d5cc83df6ecca6cec6f654f5df3ec8
shunzi <tobyzsj@gmail.com>
2024-07-02 01:18:33 +0800

    feat: install default config

--- packages/midjourney-prompt-for-eagle/src/sw.ts
+++ packages/midjourney-prompt-for-eagle/src/sw.ts
@@ -1,8 +1,21 @@
+import { setStorage } from "./utils/storage";
+import {
+  promptPositionKey,
+  tagRulesKey,
+  defaultPromptPosition,
+  defaultTagRules,
+} from "./utils/constant";
+
 async function getCurrentTabs() {
   return await chrome.tabs.query({ active: true, currentWindow: true });
 }
 
 const targetUrl = /https\:\/\/www.midjourney.com\/.*/;
+
+chrome.runtime.onInstalled.addListener(() => {
+  setStorage(promptPositionKey, defaultPromptPosition);
+  setStorage(tagRulesKey, defaultTagRules);
+});
 
 chrome.storage.onChanged.addListener(async () => {
   const tabs = await getCurrentTabs();
